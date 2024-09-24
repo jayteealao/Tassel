43053b84bd7aa9269c007246bea95920d8157dcc
jayteealao <jayteealao@gmail.com>
2024-09-24 15:19:33 +0100

    feat: Add RecentsScreen UI components and functionality
    Added UI components for RecentsScreen, including a Scaffold with a HomeAppBar and SnackbarHost.
    Implemented
     functionality to display bookmarks, handle bookmark deletion with undo action, and synchronize bookmarks with the cloud.
    Integrated FirebaseUI Auth for user sign-in and sign-out.
    Added swipe actions to edit and delete bookmarks.
    Included empty state UI for when no bookmarks are available.

--- app/src/main/java/xyz/graphitenerd/tassel/screens/recents/RecentsScreen.kt
+++ app/src/main/java/xyz/graphitenerd/tassel/screens/recents/RecentsScreen.kt
@@ -1,30 +1,33 @@
-@file:OptIn(ExperimentalMaterialApi::class)
-
-package xyz.graphitenerd.tassel
+package xyz.graphitenerd.tassel.screens.recents
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.foundation.background
+import androidx.activity.compose.LocalActivityResultRegistryOwner
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-<<<<<<< ours:app/src/main/java/xyz/graphitenerd/tassel/screens/recents/RecentsScreen.kt
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -33,80 +36,120 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import xyz.graphitenerd.tassel.R
+import xyz.graphitenerd.tassel.Screens
 import xyz.graphitenerd.tassel.model.Bookmark
 import xyz.graphitenerd.tassel.screens.AuthViewModel
-import xyz.graphitenerd.tassel.screens.recents.BookmarkViewModel
-=======
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.flow.Flow
-import xyz.graphitenerd.tassel.model.Bookmark
-import xyz.graphitenerd.tassel.model.BookmarkUiState
-import xyz.graphitenerd.tassel.model.BookmarkViewModel
-import xyz.graphitenerd.tassel.model.NewBookmarkViewModel
->>>>>>> theirs:app/src/main/java/xyz/graphitenerd/tassel/RecentsScreen.kt
 import xyz.graphitenerd.tassel.ui.*
+import xyz.graphitenerd.tassel.utils.CustomSwipeableActionsBox
+import xyz.graphitenerd.tassel.utils.SwipeAction
 
-<<<<<<< ours:app/src/main/java/xyz/graphitenerd/tassel/screens/recents/RecentsScreen.kt
+//import xyz.graphitenerd.tassel.utils.oneTimeWorkRequest
+//import xyz.graphitenerd.tassel.utils.periodicWorkRequest
+
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@OptIn(ExperimentalLifecycleComposeApi::class)
-=======
->>>>>>> theirs:app/src/main/java/xyz/graphitenerd/tassel/RecentsScreen.kt
 @Composable
 fun RecentScreen(
     bookmarkViewModel: BookmarkViewModel,
-    newBookmarkViewModel: NewBookmarkViewModel,
-    onNavigateToAddNew: () -> Unit = {}
+    authViewModel: AuthViewModel,
+    navController: NavController,
+    onNavigateToAddNew: () -> Unit = {},
 ) {
-
+    val activityResultRegistry = LocalActivityResultRegistryOwner.current?.activityResultRegistry
     val scaffoldState = rememberScaffoldState()
-
     Scaffold(
         scaffoldState = scaffoldState,
-        topBar = { HomeAppBar(onClickTasselButton = onNavigateToAddNew) },
+        topBar = {
+            HomeAppBar(
+                onClickMenuButton = {
+                    val signInLauncher = activityResultRegistry?.register(
+                        "sign-in",
+                        FirebaseAuthUIActivityResultContract(),
+                    ) { res ->
+                        authViewModel.onSignInResult(res)
+                    }
+                    if (FirebaseAuth.getInstance().currentUser == null) {
+                        signInLauncher?.launch(authViewModel.signInIntent)
+                    } else {
+                        authViewModel.accountService.signOut()
+                    }
+//                                    bookmarkViewModel.loadJsonBookmarks()
+                    // TODO: add else branch saying already logged in
+                },
+                onClickActionButton = onNavigateToAddNew
+            )
+                 },
+        snackbarHost = { snackbarHostState ->
+            SnackbarHost(hostState = snackbarHostState) {
+                Snackbar(
+                    modifier = Modifier.padding(bottom = 96.dp),
+                    snackbarData = it
+                )
+            }
+        }
     ) {
-        RecentsScreenContent(
-            bookmarksFlow = bookmarkViewModel.bookmarks
+
+        val snackbarState by bookmarkViewModel.deletedBookmark.collectAsStateWithLifecycle(lifecycle = LocalLifecycleOwner.current.lifecycle)
+        val count by bookmarkViewModel.bookmarkCount.collectAsStateWithLifecycle(lifecycle = LocalLifecycleOwner.current.lifecycle)
+
+        LaunchedEffect(true) {
+            bookmarkViewModel.syncBookmarksToCloud()
+        }
+
+//        TODO: if a bookmark deletion is undone, trying to undelete it again doesnt show the snackbar
+        LaunchedEffect(snackbarState) {
+            var result: SnackbarResult? = null
+            if (snackbarState != null) {
+                result = scaffoldState.snackbarHostState.showSnackbar("Bookmark Deleted", "UNDO")
+            }
+            if (result == SnackbarResult.ActionPerformed) {
+                withContext(Dispatchers.IO) {
+                    bookmarkViewModel.addBookmark(snackbarState!!)
+                }
+            }
+        }
+        RecentScreenContent(
+            bookmarksFlow = bookmarkViewModel.bookmarks,
+            navController = navController,
+            deleteAction = { bookmarkViewModel.deleteBookmark(it) },
+            count
         )
     }
 }
 
 @Composable
-fun RecentsScreenContent(bookmarksFlow: Flow<List<Bookmark>>) {
-    val bookmarks: State<List<Bookmark>> = bookmarksFlow.collectAsState(emptyList<Bookmark>())
-    if (bookmarks.value.isEmpty()) {
+fun RecentScreenContent(
+    bookmarksFlow: Flow<List<Bookmark>>,
+    navController: NavController,
+    deleteAction: (Bookmark) -> Unit = {},
+    bookmarkCount: Int = 0
+) {
+    val bookmarks: State<List<Bookmark>> = bookmarksFlow.collectAsState(emptyList())
+    if (bookmarkCount == 0) {
         EmptyBookmarkFolder()
     } else {
-        Contents(bookmarks.value)
+        Contents(bookmarks.value, navController, deleteAction)
     }
 }
 
 @Composable
-fun RecentsScreenContent(uiState: List<Bookmark>) {
+fun RecentScreenContent(uiState: List<Bookmark>) {
     if (uiState.isEmpty()) {
         EmptyBookmarkFolder()
     } else {
-        Contents(uiState)
+//        Contents(uiState)
     }
 }
 
 @Composable
-private fun BottomNavBar(uiState: BookmarkUiState) {
-    Box(
-        modifier = Modifier
-            .background(color = Color(0xff, 0xff, 0xff, 128))
-            .fillMaxWidth()
-            .height(72.dp),
-        contentAlignment = Alignment.TopCenter
-    ) {
-        BottomNavButton(state = uiState.bottomNavBarState)
-    }
-}
-
-@Composable
-private fun Contents(bookmarks: List<Bookmark>) {
+private fun Contents(
+    bookmarks: List<Bookmark>,
+    navController: NavController,
+    deleteAction: (Bookmark) -> Unit = {},
+) {
+    val scope = rememberCoroutineScope()
     LazyColumn(
-        contentPadding = PaddingValues(20.dp, 0.dp, 20.dp, 72.dp)
+        contentPadding = PaddingValues(20.dp, 0.dp, 20.dp, 78.dp)
     ) {
         item {
 
@@ -120,12 +163,42 @@ private fun Contents(bookmarks: List<Bookmark>) {
                 color = Color.Black,
             )
         }
-        items(bookmarks) { bookmark ->
+        items(bookmarks, key = { it.id }) { bookmark ->
 //            Log.e("tassel", "in recent screen column, current bookmark $bookmark")
-            BookmarkCard(bookmark = bookmark)
-            Divider(
-                color = Color.Black,
+
+            val delete = SwipeAction(
+                icon = painterResource(id = R.drawable.icoutlinedelete),
+                background = Color.Red,
+                onSwipe = {
+                    scope.launch(Dispatchers.IO) {
+                        deleteAction(bookmark)
+                    }
+                }
             )
+
+            val edit = SwipeAction(
+                icon = rememberVectorPainter(Icons.Default.Edit),
+                background = Color(0xFF7CB9E8),
+                onSwipe = {
+                    Log.d("edit", "${Screens.ADDNEW.name}?id=${bookmark.id}")
+
+                    navController.navigate("${Screens.ADDNEW.name}?id=${bookmark.id}")
+                }
+            )
+
+            CustomSwipeableActionsBox(
+                modifier = Modifier,
+                startActions = listOf(edit),
+                endActions = listOf(delete),
+                swipeThreshold = 96.dp,
+                backgroundUntilSwipeThreshold = Color.White
+            ) {
+                BookmarkCard(bookmark = bookmark)
+                Divider(
+                    modifier = Modifier.align(Alignment.BottomCenter),
+                    color = Color.Black,
+                )
+            }
         }
     }
 }
